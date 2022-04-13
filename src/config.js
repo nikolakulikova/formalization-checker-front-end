ccef54e540534798ac3b63605f86b15269880a46
nikolakulikova <kulikova.n12345@gmail.com>
2022-04-13 13:16:52 +0200

    changed path to redirect to github

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 module.exports = {
      BACKEND_URL: process.env.REACT_APP_BACKEND_URL,
-     CLIENT_ID: process.env.CLIENT_ID,
-     REDIRECT: process.env.REDIRECT,
+     CLIENT_ID: process.env.REACT_APP_CLIENT_ID,
+     REDIRECT: process.env.REACT_APP_REDIRECT,
 }
 
