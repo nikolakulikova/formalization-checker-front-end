cc9f3660ddfac6ce05012db64701dce0fc290500
nikolakulikova <kulikova.n12345@gmail.com>
2022-04-20 13:07:25 +0200

    Updates

---
+++ precache-manifest.533ae8c0fb027d9f64d48444197a5230.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8cefc23cc24fb1cd9a8b7577afa6c5d",
+    "url": "/formalization-checker/index.html"
+  },
+  {
+    "revision": "a7cd76c288031bc9695d",
+    "url": "/formalization-checker/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "e1f3bcd48297a58e1004",
+    "url": "/formalization-checker/static/css/main.b7de0216.chunk.css"
+  },
+  {
+    "revision": "a7cd76c288031bc9695d",
+    "url": "/formalization-checker/static/js/2.d08d766f.chunk.js"
+  },
+  {
+    "revision": "b9aa32ad4d98fbb6880a92ba641f2f93",
+    "url": "/formalization-checker/static/js/2.d08d766f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1f3bcd48297a58e1004",
+    "url": "/formalization-checker/static/js/main.5086deb7.chunk.js"
+  },
+  {
+    "revision": "a4d4a3bfe42b321e7221",
+    "url": "/formalization-checker/static/js/runtime-main.4ff20a1a.js"
+  }
+]);
